4c53d22af7c65842a25bcb31c48cd3e4807c981b
Raihan OiOi <rayaindonesia599@gmail.com>
2025-07-03 12:32:13 +0700

    test 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <h1 className="font-bold text-2xl">Hello World</h1>
-      <h2 className='font-bold text-xl'>Uji coba</h2>
+      <h3 className='font-bold text-xl'>Uji coba</h3>
     </div>
   )
 }
